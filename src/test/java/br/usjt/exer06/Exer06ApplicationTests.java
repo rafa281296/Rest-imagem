0407e0e82c7ab6c1c8d17609e01fd943156bf389
Top <Top@DESKTOP-HCCJG5P>
2020-06-06 18:25:45 -0300

    Commit

---
+++ src/test/java/br/usjt/exer06/Exer06ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.exer06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exer06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
